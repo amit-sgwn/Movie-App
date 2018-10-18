ce87b47e30fb5bb64fbcbd913f0632d193fd2053
amit-sgwn <amitsangwan898@gmail.com>
2018-10-18 12:02:10 +0530

    :art: Add Moya target Movie

---
+++ Movie App/Networking/MovieDataBaseService.swift
@@ -0,0 +1,19 @@
+//
+//  MovieDataBaseService.swift
+//  Movie App
+//
+//  Created by Amit Kumar on 18/10/18.
+//  Copyright © 2018 Box8. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+
+public enum MovieDB {
+    //1
+    static private let publicKey = ""  //put your keys here
+    static private let privateKey = "" // put your private keys here
+    // 2
+    case movie
+}
