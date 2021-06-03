8d5d3b7111a18312a66947c50bee1d866440cff7
Amit kumar <amitsangwan898@gmail.com>
2021-06-03 13:24:15 +0530

    Create NerworkUtility.swift
    Add new file for networking

---
+++ Movie App/Networking/NerworkUtility.swift
@@ -0,0 +1,44 @@
+import Foundation
+
+
+//MARK: Utility for api call
+struct HttpUtility {
+    
+    func getApiData<T: Decodable>(requestURL: URL, resultType: T.Type,completionHandler: @escaping(_ result: T) -> Void) {
+        debugPrint(requestURL)
+        URLSession.shared.dataTask(with: requestURL){ (responseData, httpUrlResponse, error) in
+            if error == nil && responseData != nil && responseData?.count != 0 {
+                //Parsing data from response
+                let decoder = JSONDecoder()
+                do {
+                    let result = try decoder.decode(T.self, from: responseData!)
+                    completionHandler(result)
+                } catch let error {
+                    debugPrint(error.localizedDescription)
+                }
+            }
+            
+        }.resume()
+    }
+    
+    func getApiData<T: Decodable>(requestURL: URLRequest, resultType: T.Type  ,completionHandler: @escaping(_ result: T) -> Void) {
+        
+        URLSession.shared.dataTask(with: requestURL){ (responseData, httpUrlResponse, error) in
+            if error == nil && responseData != nil && responseData?.count != 0 {
+                //Parsing data from response
+                let decoder = JSONDecoder()
+                do {
+                    let result = try decoder.decode(T.self, from: responseData!)
+                    completionHandler(result)
+                } catch let error {
+                    debugPrint(error.localizedDescription)
+                }
+            }
+            
+        }.resume()
+    }
+}
+
+
+
+
